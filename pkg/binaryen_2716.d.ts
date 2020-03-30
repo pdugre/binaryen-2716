dd4bce1ac18a5e92e51f08d3e3278c994c33de7c
pdugre <pdugre@devolutions.net>
2020-03-30 16:52:57 -0400

    added built wasm on lfs

---
+++ pkg/binaryen_2716.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @returns {Uint8Array} 
+*/
+export function test_binaryen(): Uint8Array;
---
+++ pkg/binaryen_2716_bg.d.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function test_binaryen(a: number): void;
+export function __wbindgen_free(a: number, b: number): void;
